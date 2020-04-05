6762d71b38ce30231841ee508b1fa1bbbc7475f8
Diego Francisco <deliasfrancisco@gmail.com>
2020-04-05 11:38:07 -0300

    #initial commit

---
+++ WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Title = "Aplicação ASP.NET MVC - Diego";
+
+			ViewBag.DataEHora = DateTime.Now;
+
+			return View();
+		}
+	}
+}
